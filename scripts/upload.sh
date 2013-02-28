08a73b69042e32d20321230d37caa30875893ce8
Daneel S. Yaitskov <rtfm.rtfm.rtfm@gmail.com>
2013-02-28 14:35:38 +0400

    fix upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,4 +2,4 @@
 
 # upload php sources
 #scp -r -i $(dirname $0)/../dev1.pem  $(dirname $0)/../wroot/*  ubuntu@repodev1.malov.net:/home/ubuntu/bluesite
-rsync -avz -e "ssh -i $(dirname $0)/../dev1.pem" $(dirname $0)/../wroot/  ubuntu@repodev1.malov.net:/home/ubuntu/bluesite/
+rsync -avz -e "ssh -i $(dirname $0)/../dev1.pem" $(dirname $0)/../wroot/  ubuntu@repodev1.malov.net:/home/ubuntu/bluesite/wroot/
